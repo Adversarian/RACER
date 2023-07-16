095a5e39100a53ed39a54e9ec31136665f723439
Adversarian <a77physics@gmail.com>
2023-07-16 14:35:48 +0330

    Added versioning info and setup file.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+exec(open("RACER/version.py").read())
+
+setup(
+    name="pyracer",
+    packages=find_packages(),
+    version=__version__,
+    license="MIT",
+    description="Unofficial Python implementation of the RACER classification algorithm.",
+    author="Adversarian",
+    author_email="a77physics@gmail.com",
+    url="https://github.com/Adversarian/RACER",
+    long_description_content_type="text/markdown",
+    keywords=["machine learning", "classification", "RACER"],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "optbinning",
+        "scikit-learn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Machine Learning",
+    ],
+)
