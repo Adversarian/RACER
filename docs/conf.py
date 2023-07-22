2e5bf8cac01c1bc0e0edea81420d160519585159
Adversarian <a77physics@gmail.com>
2023-07-22 12:50:42 +0330

    Doc: Change to RTD theme.

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -31,7 +32,7 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Options for todo extension ----------------------------------------------
